50e7d384913c6d0c54b6bdf057852c1876b85e95
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-28 22:32:58 -0700

    removed duplicate code

--- js/utils/generateMarkdown.js
+++ js/utils/generateMarkdown.js
@@ -1,20 +1,5 @@
 const fs = require('fs');
 
-const writeFile = fileContent => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile('./dist/index.html', fileContent, err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve({
-        ok: true,
-        message: 'File created!'
-      });
-    });
-  });
-};
 // // TODO: Create a function that returns a license badge based on which license is passed in
 // // If there is no license, return an empty string
 // function renderLicenseBadge(license) {}
