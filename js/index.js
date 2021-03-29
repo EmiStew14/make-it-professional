68e75deb80d210028e34ccedb76dcf516fee93f7
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-28 22:31:14 -0700

    added markdown template and cleaned up questions

--- js/index.js
+++ js/index.js
@@ -1,99 +1,200 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 const { writeFile, copyFile } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-return inquirer
-.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is the name of your project? (Required)',
-    validate: nameInput => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log('You need to enter a project name!');
-        return false;
-      }
-    }
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of the project (Required)',
-    validate: descriptionInput => {
-      if (descriptionInput) {
-        return true;
-      } else {
-        console.log('You need to enter a project description!');
-        return false;
-      }
-    }
-  },
-  {
-    type: 'checkbox',
-    name: 'Table of Contents',
-    message: 'Provide a table of contents of the project (Optional)',
-    choices:['Installation', 'Usage','Credits','License'],
-  },
-  {
-    type: 'input',
-    name: 'Installation',
-    message: 'Provide a description of how to install the project',
-    validate: installationInput => {
-      if (installationInput) {
-        return true;
-      } else {
-        console.log('You need to enter a installation steps!');
-        return false;
-      }
-    }
-  },
-  {
-    type: 'input',
-    name: 'Usage',
-    message: 'Provide an explanation of how to use your project and include screenshots',
-    validate: usageInput => {
-      if (usageInput) {
-        return true;
-      } else {
-        console.log('You need to enter a usage description!');
-        return false;
-      }
-    }
-  },
-  {
-    type: 'input',
-    name: 'Credits',
-    message: 'List your collaborators, if any, with links to their GitHubs profiles',
-    validate: usageInput => {
-      if (usageInput) {
-        return true;
-      } else {
-        console.log('You need to enter a usage description!');
-        return false;
-      }
-    }
-  },
-  {
-    type: 'list',
-    name: 'License',
-    message: 'Choose a license for your project',
-    default: false
-  },
-]);
-};
+ const questions = [];
 
+ //Contact section 
+ const reachMe = () => {
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name? (Required)',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub Username (Required)',
+          validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub username!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
+        },
+        {
+          type: 'input',
+          name: 'about',
+          message: 'Provide some information about yourself:',
+          when: ({ confirmAbout }) => confirmAbout
+        }
+      ])
+      .then(questions => {
+        questions.push(data);
+      });
+    };
 // TODO: Create a function to write README file
 const writeToFile =(fileName, data) => {
-   
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'toc',
+        message: 'Provide a table of contents of the project (Optional)',
+        choices:['Installation', 'Usage','Credits','License'],
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'Provide a description of how to install the project',
+        validate: installationInput => {
+          if (installationInput) {
+            return true;
+          } else {
+            console.log('You need to enter a installation steps!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide an explanation of how to use your project and include screenshots',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('You need to enter a usage description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'creditsConfirm',
+        message: 'Is there anyone else who helped this project?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'Credits',
+        message: 'List your collaborators with their GitHub usernames',
+        when: ({ creditsConfirm }) => creditsConfirm,
+        validate: creditInput => {
+            if (creditInput) {
+              return true;
+            } else {
+              console.log('You need to enter a Github Username!');
+              return false;
+            }
+          }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddMore',
+        message: 'Would you like to enter another collaborator?',
+        default: false
+      },
+      {
+        type: 'input',
+        name: 'License',
+        message: 'Input a license for your project',
+        default: true,
+      },
+    ])
+    .then( data => {
+    if (data.confirmAddMore) {
+        return (data.Credits);
+      } else {
+        return data;
+      }
+    })
+    .then((data) => {
+      
+        fs.writeFile('/.README.md', data, (err) => {
+      
+          if (err) {
+            return console.log(err);
+          }
+      
+          console.log("File Created!");
+      
+        });
+})
+.catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+});
 }
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+init()
+    .then(writeToFile)
+    .then(data => {
+     return generateMarkdown(data);
+   })
+   .then(pageHTML => {
+     return writeFile(pageHTML);
+   })
+//   .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+//   })
+//   .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+//   })
+   .catch(err => {
+     console.log(err);
+   });
--- js/utils/generateMarkdown.js
+++ js/utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
 
-`;
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${data.title}</h1>
+    <h1>${data.description}</h1>
+    <h1>${data.toc}</h1>
+      <ul> 
+      <li>${data.toc.choices}</li>
+    <h1>${data.installation}</h1>
+    <h1>${data.usage}</h1>
+    <h1>${data.Credits}</h1>
+    <h1>${data.License}</h1>
+    <h2><a href="https://github.com/${data.Credits}">Github</a></h2>
+  </body>
+  </html>
+  `;
 }
 
 module.exports = generateMarkdown;
